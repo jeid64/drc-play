7da59aa1d1f2c510ff2ef09527b450e79fec4bc6
Julien Eid <jeid64@gmail.com>
2015-02-11 17:22:15 -0500

    Fixed IDR request message.

--- drc-cap.cc
+++ drc-cap.cc
@@ -47,28 +47,32 @@
 #include "sdl-handler.h"
 
 void send_msg(){
+    struct sockaddr_in si_other;
+    int s, i, slen=sizeof(si_other);
 
-  int sock_fd_;
+    const char* message =  "\1\0\0\0";
 
-  sockaddr_in sin;
-  memset(&sin, 0, sizeof (sin));
-  sin.sin_family = AF_INET;
-  sin.sin_port = htons(50010);
-  inet_aton("192.168.1.10", &sin.sin_addr);
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        fprintf(stderr, "Failed to open MSG stream socket. \n");
+    }
 
-  sock_fd_ = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (sock_fd_ == -1) {
-    printf("Fuck \n");
-  }
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(50010); // Wii U MSG port.
 
+    if (inet_aton("192.168.1.10" , &si_other.sin_addr) == 0)
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
 
-    sockaddr_in sender;
-    socklen_t sender_len = sizeof (sender);
-    int msg_max_size = 2048;
-    const char* message = "\1\0\0\0";
-    sendto(sock_fd_, message,sizeof(message),0,reinterpret_cast<sockaddr*>(&sender),sender_len);
-    printf("sent\n");
+    if (sendto(s, message, sizeof(uint32_t) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+    {
+        fprintf(stderr, "Failed to send IDR request message.\n");
+    }
 
+    close(s);
 }
 
 int main(int argc, char** argv) {
